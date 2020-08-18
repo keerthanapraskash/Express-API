2629fdd8fe1081dfc880d37b843d00902b6d854d
keerthanapraskash <keerthanaprakash7403@gmail.com>
2020-08-18 21:20:29 +0530

    second commit

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
+
+app.set('template engine','pug');
+
 app.get('/',function(req,res)
 {
-	res.send("hello world");
+	res.send("Hello World");
 
 });
+
+app.get('/index',function(req,res)
+{
+	res.render('template/index',{title:"api",message:"This is my first API"});
+
+});
+
+
 var server = app.listen(PORT, function(err)
 {
 	if(err) console.log("error in loading server")
