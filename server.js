8401859215227151d86873013ed02f610275704f
keerthanapraskash <keerthanaprakash7403@gmail.com>
2020-08-19 14:08:29 +0530

    Added a view folder

--- server.js
+++ server.js
@@ -3,23 +3,55 @@ var app = express();
 var PORT = 3000;
 
 
-app.set('template engine','pug');
+app.set('view engine','pug');
 
 app.get('/',function(req,res)
 {
-	res.send("Hello World");
+        res.send("Hello World");
 
 });
 
 app.get('/index',function(req,res)
 {
-	res.render('template/index',{title:"api",message:"This is my first API"});
+        res.render('index',{title:"api",message:"This is my first API"});
 
 });
 
 
 var server = app.listen(PORT, function(err)
 {
-	if(err) console.log("error in loading server")
-	console.log("listening to PORT",PORT);
+        if(err) console.log("error in loading server")
+        console.log("listening to PORT",PORT);
 });
+
+
+
+
+// var express = require("express");
+// var app = express();
+// var PORT = 3000;
+
+
+// app.set('view engine','pug');
+
+// app.get('/',function(req,res)
+// {
+//         res.send("Hello World");
+
+// });
+
+// app.get('/index',function(req,res)
+// {
+//         res.render('index',{title:"api",message:"This is my first API"});
+
+// });
+
+
+// var server = app.listen(PORT, function(err)
+// {
+//         if(err) console.log("error in loading server")
+//         console.log("listening to PORT",PORT);
+// });
+// Amalaa
+// var express = require("express"); var app = express(); var PORT = 3000; app.set('view engine','pug'); app.get('/',function(re
+// ith server.js il copy paste cheyy
